ba9134d2c4253aa752161573e51c7838d96da117
Owl <ad-2812@yandex.com>
2018-09-03 08:25:22 -0400

    Add files via upload
    +

---
+++ File.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class File
+    {
+        public File()
+        {
+
+        }
+
+        protected string filename, path;
+        protected DataTable data = new DataTable();
+        protected bool hasChanged = false;
+
+        internal void Create(File fileCreate, string filename, string path)
+        {
+
+
+            fileCreate.Filename = filename;
+            fileCreate.Path = path;
+            fileCreate.Data = new DataTable();
+
+            Console.WriteLine($"File name : {fileCreate.Filename} . Path : {fileCreate.Path}  File has been changed : {fileCreate.HasChanged.ToString()}");
+            Console.ReadKey();
+        }
+
+        internal void Save(string newname)
+        {
+            this.Filename = newname;
+            
+        }
+        internal void Show()
+        {
+            Console.WriteLine($"File name : {this.Filename} . Path : {this.Path}  File has been changed : {this.HasChanged.ToString()}");
+        }
+        public string Filename
+        {
+            get { return filename; }
+            set
+            {
+                if (this.filename != value)
+
+                {
+                    this.filename = value;
+                    this.hasChanged = true;
+                }
+                
+            
+            }
+        }
+
+        public string Path
+        {
+            get { return path; }
+            set { path = value; }
+
+        }
+
+        public DataTable Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+        public bool HasChanged
+        {
+            get { return hasChanged; }
+            set
+            {
+                HasChanged = value;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File smpx = new File();
+
+            smpx.Create(smpx,"music.smpx", "% userprofile %\\library\\");
+            Console.ReadKey();
+
+            smpx.Save("i need you too.smpx");
+            Console.ReadKey();
+
+            smpx.Show();
+            Console.ReadKey();
+                       
+           
+        }
+        
+         
+        
+    }
+}
